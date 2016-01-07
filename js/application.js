2cf3acf3a12aacc2f26f8a9b1fa9c4fd80f7491f
Austin Gatlin and Tim Labeeuw <pair+agatlin+tlabeeuw@pivotal.io>
2016-01-06 17:45:19 -0800

    Get leslie spinning and facing the same way

--- js/application.js
+++ js/application.js
@@ -23,10 +23,29 @@ function init() {
 
   var loader = new THREE.TextureLoader();
   loader.load(leslies[0].name, function ( texture ) {
-    var geometry = new THREE.BoxGeometry( 256, 512, 32, 1, 1, 4 );
-    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
-    var mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    var geometry = new THREE.BoxGeometry( 256, 64, 512, 1, 1, 5);
+    var frontLeslieMaterial = new THREE.MeshBasicMaterial({map: texture});
+    var backTexture = texture.clone();
+    backTexture.flipY = false;
+    backTexture.needsUpdate = true;
+    var backLeslieMaterial = new THREE.MeshBasicMaterial({map: backTexture});
+    var topMaterial = new THREE.MeshBasicMaterial({color: '#' + leslies[0].colors[0]});
+    var bottomMaterial = new THREE.MeshBasicMaterial({color: '#' + leslies[0].colors[4]});
+    var sideMaterial = new THREE.MeshBasicMaterial({vertexColors: THREE.FaceColors});
+    var materials = [
+      sideMaterial,
+      sideMaterial,
+      frontLeslieMaterial,
+      backLeslieMaterial,
+      topMaterial,
+      bottomMaterial,
+    ];
+    for(var i = 0; i < 20; i++) {
+      var colorIndex = Math.floor(i / 2) % 5;
+      geometry.faces[i].color.set( new THREE.Color('#' + leslies[0].colors[colorIndex]) );
+    }
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+    scene.add(mesh);
   });
 
   var canvas = document.createElement( 'canvas' );
@@ -42,7 +61,9 @@ function init() {
 
 function animate() {
   requestAnimationFrame( animate );
-  scene.rotation.y += 0.01;
+  scene.rotation.y += 0.001;
+  scene.rotation.x += 0.01;
+  scene.rotation.z += 0.001;
   renderer.render( scene, camera );
 }
 
